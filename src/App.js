3cfd8074905d101f776aed56e4e050c3e44a7b97
quanghoath <hoanqfpt@gmail.com>
2020-10-13 17:03:33 +0700

    add new file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Row from './Row';
+import requests from './requests';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hey Clever Programer! Let's build netflix-clone today.</h1>
+      <Row title="NETFLIX ORINGINALS" fetUrl={requests.fetchTrending}/>
+      <Row title="Trending now" fetUrl={requests.fetchTrending} />
     </div>
   );
 }
